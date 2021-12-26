1f187af3a60fe2a436f2049336a43f7b618e58dd
tford-dev <terranceford513@gmail.com>
2021-12-26 07:45:27 -0500

    First commit, set up files and adjusted margin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			{/*Spotify logo*/}
+			{/*Login with spotify button*/}
+		</div>
+	);
+}
+
+export default App;
