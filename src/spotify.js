0bc2a4795569cc77f09c125f4c7de299050b9645
tford-dev <terranceford513@gmail.com>
2021-12-27 18:26:29 -0500

    Updated client id and redirect

--- src/spotify.js
+++ src/spotify.js
@@ -1,9 +1,9 @@
 //https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
 //38bbf18b772444b8b19c7c9b270d341f
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "http://61ca093d1fd3341567318aff--tford-dev-spotify.netlify.app/";
+const redirectUri = "https://61ca093d1fd3341567318aff--tford-dev-spotify.netlify.app/";
 //id given by spotify for developers link
-const clientId = "4d39802433fb43098b20e5c279cc2a46";
+const clientId = "38bbf18b772444b8b19c7c9b270d341f";
 
 //Permissions of app from API;
 const scopes = [
